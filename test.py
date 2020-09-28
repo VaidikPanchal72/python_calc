59b0bbc503af27cfe4e422eaef9cac0e11eb48e5
VaidikPanchal72 <68104160+VaidikPanchal72@users.noreply.github.com>
2020-09-28 14:18:07 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import sys
 
 from PyQt5.QtWidgets import QApplication
@@ -14,4 +15,22 @@ helloMsg.move(60, 30)
 
 window.show()
 
+=======
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle('PyQt5 App')
+window.setGeometry(100, 100, 280, 80)
+window.move(600, 15)
+helloMsg = QLabel('This is Test', parent=window)
+helloMsg.move(60, 30)
+
+window.show()
+
+>>>>>>> 9c6c2a5d95b5d0d6a406cc9c3739fab555238ca7
 sys.exit(app.exec_())
